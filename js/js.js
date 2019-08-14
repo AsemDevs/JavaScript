2151e89eb9f146fcbcce1d3b5194c4d61001ab3f
AsemDevs <asemhamdi97@gmail.com>
2019-08-14 18:09:47 +0100

    initial commit

---
+++ js/js.js
@@ -0,0 +1,12 @@
+function changeFontSize(value){
+	document.getElementById('demo').style.fontSize = value+'px';
+}
+
+
+function onBulb(){
+	document.getElementById('myImage').src='pic_bulbon.gif' 
+}
+
+function offBulb(){
+	document.getElementById('myImage').src='pic_bulboff.gif' 
+}
