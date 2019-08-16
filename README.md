b6afe9ac3b454fc0a30804f0303f3dc00d1e08c9
AsemDevs <asemhamdi97@gmail.com>
2019-08-16 22:15:45 +0100

    Temperature converter

---
+++ README.md
@@ -0,0 +1,20 @@
+# My practices
+
+Here i'm going to share my learning process and practices
+
+## Lists of what i'll do
+
+* Small coding practices
+
+* a new feature I learned
+
+* every thing I learn that i find it useful to keep and share
+
+* *Every commit will represent an item of the above*
+    *(as instance Every feature i learned will be represented in a commit)*
+
+### Notes to know
+
+I may update commit(Feature I learned) if i felt that there is better way to do it
+
+and I will do that by altering the commit this was as example: Update 1: a commit
--- js/js.js
+++ js/js.js
@@ -1,12 +1,26 @@
-function changeFontSize(value){
-	document.getElementById('demo').style.fontSize = value+'px';
+function toCelsius(fahrenheit){
+    fahrenheit = window.prompt("Enter a degree in Fahrenheit");
+    /* 
+    trim method will remove the white spaces in the input if there is any
+    and compare with the empty string 
+    to check wether the user entered a value or not
+    */
+    if (fahrenheit.trim() == ''){
+        fahrenheit = alert("You didn't enter a value");
+    }
+    if (fahrenheit.trim() != ''){
+        result = (5/9) * (fahrenheit-32);
+        document.getElementById("result").innerHTML = result + " &deg;C"
+    }
 }
 
-
-function onBulb(){
-	document.getElementById('myImage').src='./imgs/pic_bulbon.gif' 
-}
-
-function offBulb(){
-	document.getElementById('myImage').src='./imgs/pic_bulboff.gif' 
+function toFahrenheit(celsius){
+    celsius = window.prompt("Enter a degree in Celsius");
+    if (celsius.trim() == ''){ 
+        celsius = alert("You didn't enter a value");
+    }
+    if (celsius.trim() != ''){
+        result = (celsius * (9/5)) +32;
+        document.getElementById("result").innerHTML = result + " &deg;F"
+    }
 }
