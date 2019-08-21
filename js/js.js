800e56c946dc4ebadca47bbe501a764dec8cac4e
AsemDevs <asemhamdi97@gmail.com>
2019-08-21 21:18:43 +0100

    Min & max values from a list

--- js/js.js
+++ js/js.js
@@ -1,26 +1,26 @@
-function toCelsius(fahrenheit){
-    fahrenheit = window.prompt("Enter a degree in Fahrenheit");
-    /* 
-    trim method will remove the white spaces in the input if there is any
-    and compare with the empty string 
-    to check wether the user entered a value or not
-    */
-    if (fahrenheit.trim() == ''){
-        fahrenheit = alert("You didn't enter a value");
+var values = [40, 100, 1, 5, 25, 10];
+document.getElementById("demo").innerHTML = maxValue(values);
+document.getElementById("demo1").innerHTML = minValue(values);
+
+
+function maxValue(arr) {
+  var len = arr.length;
+  var max = -Infinity;
+  while (len--) {
+    if (arr[len] > max) {
+      max = arr[len];
     }
-    if (fahrenheit.trim() != ''){
-        result = (5/9) * (fahrenheit-32);
-        document.getElementById("result").innerHTML = result + " &deg;C"
-    }
+  }
+  return max;
 }
 
-function toFahrenheit(celsius){
-    celsius = window.prompt("Enter a degree in Celsius");
-    if (celsius.trim() == ''){ 
-        celsius = alert("You didn't enter a value");
+function minValue(arr) {
+    var len = arr.length;
+    var min = Infinity;
+    while (len--) {
+      if (arr[len] < min) {
+        min = arr[len];
+      }
     }
-    if (celsius.trim() != ''){
-        result = (celsius * (9/5)) +32;
-        document.getElementById("result").innerHTML = result + " &deg;F"
-    }
-}
+    return min;
+  }
